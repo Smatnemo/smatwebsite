bad900421da948c0685c63dcb0f1726670382e7d
Tertese Samuel Moses <smatwebsite@meet.smatwebsite.com>
2023-09-20 16:04:09 +0100

    Failed to make so Many Changes

--- smatwebsite/wsgi.py
+++ smatwebsite/wsgi.py
@@ -6,8 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
+import os, sys, site
 
-import os
+sys.path.append('/var/www/www.smatwebsite.co.uk/smatwebsite')
+sys.path.append('/var/www/www.smatwebsite.co.uk/smatwebsite/smatwebsite')
+sys.path.append('/var/www/www.smatwebsite.co.uk/smatwebsite/venv/lib/python3.10/site-packages')
+
 
 from django.core.wsgi import get_wsgi_application
 
