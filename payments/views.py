0f0efab3520bad1ddb571674a9654ad6c25b9856
Tertese Moses <Smatnemo25@gmail.com>
2023-10-01 14:57:10 +0100

    Created two views for Home Pay and stripe config

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+# Importing modules to create XHR request
+from django.http.response import JsonResponse 
+from django.views.decorators.csrf import csrf_exempt 
+from django.conf import settings
 
 # Create your views here.
+class HomePayView(TemplateView):
+    template_name = 'payHome.html'
+
+@csrf_exempt
+def stripe_config(request):
+    if request.method == 'GET':
+        stripe_config = {'publicKey': settings.STRIPE_PUBLISHABLE_KEY}
+        return JsonResponse(stripe_config, safe=False)
+    
+
