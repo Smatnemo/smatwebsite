a6adc936db14a137a29932e1ad1e7774b0cfa073
Tertese Moses <Smatnemo25@gmail.com>
2023-10-01 16:40:34 +0100

    Created Checkout session

--- payments/urls.py
+++ payments/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('payment/', views.HomePayView.as_view(), name='payHome'),
     path('config/', views.stripe_config),
+    path('create-checkout-session/', views.create_checkout_session),
 ]
