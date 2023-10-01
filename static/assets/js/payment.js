aa25cb0e69be325d573c3e983e4d56819dcb4c55
Tertese Moses <Smatnemo25@gmail.com>
2023-10-01 16:05:22 +0100

    Updated the page

--- static/assets/js/payment.js
+++ static/assets/js/payment.js
@@ -2,7 +2,8 @@
 // use fetch api to make XHR request to config/
 // Get publishable key
 fetch("/config/")
-.then((result) => { return result.json(); })
+.then((result) => { 
+    return result.json(); })
 .then((data) => {
     // Initialize Stripe.js
     // its properties can be added, updated, or removed
