d3f6288eab52a6f8ba983f5d9c3e804c2072aa8b
Tertese Moses <Smatnemo25@gmail.com>
2023-10-01 14:30:52 +0100

    Created a js and js/payment.js file

---
+++ static/assets/js/payment.js
@@ -0,0 +1,10 @@
+// console.log("Sanity check!");
+// use fetch api to make XHR request to config/
+// Get publishable key
+fetch("/config/")
+.then((result) => { return result.json(); })
+.then((data) => {
+    // Initialize Stripe.js
+    // its properties can be added, updated, or removed
+    const stripe = Stripe(data.publicKey);
+});
