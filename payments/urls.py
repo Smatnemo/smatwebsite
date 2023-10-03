8af4292ceeb65caf7ff95026faf4c1488e3df9a8
Tertese Moses <Smatnemo25@gmail.com>
2023-10-03 07:26:53 +0100

    Created url paths for success and cancelled

--- payments/urls.py
+++ payments/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('payment/', views.HomePayView.as_view(), name='payHome'),
     path('config/', views.stripe_config),
     path('create-checkout-session/', views.create_checkout_session),
+    path('success/', views.SuccessView.as_view(), name='successfulPayment'),
+    path('cancelled/', views.CancelledView.as_view(), name='cancelledPayment')
 ]
