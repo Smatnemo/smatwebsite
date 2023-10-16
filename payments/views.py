5dc3862105578b1d0b14206fe2120755be1ab74a
Tertese Moses <Smatnemo25@gmail.com>
2023-10-16 20:36:15 +0100

    Created views for two products

--- payments/views.py
+++ payments/views.py
@@ -26,23 +26,28 @@ def stripe_config(request):
         return JsonResponse(stripe_config, safe=False)
 
 @csrf_exempt
-def create_checkout_session(request):
-    if request.method == 'GET':
-        domain_url = '127.0.0.1:8000/'
-        stripe.api_key = settings.STRIPE_SECRET_KEY
-        try:
-            # Create new Checkout Session for the order
-            # Other optional params include:
-            # [billing_address_collection] - to display billing address details on the page
-            # [customer] - if you have an existing Stripe Customer ID
-            # [payment_intent_data] - capture the payment later
-            # [customer_email] - prefill the email input in the form
-            # For full details see https://stripe.com/docs/api/checkout/sessions/create
+def create_checkout_session(request, product):
+    # A list of the available products
+    domain_url = 'https://www.smatwebsite.co.uk/'
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    success_url=domain_url + 'success?session_id={CHECKOUT_SESSION_ID}'
+    cancel_url=domain_url + 'cancelled/'
 
-            # ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
+    # Create new Checkout Session for the order
+    # Other optional params include:
+    # [billing_address_collection] - to display billing address details on the page
+    # [customer] - if you have an existing Stripe Customer ID
+    # [payment_intent_data] - capture the payment later
+    # [customer_email] - prefill the email input in the form
+    # For full details see https://stripe.com/docs/api/checkout/sessions/create
+
+    # ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
+    if request.method == 'GET' and product == 'coffee':
+        
+        try:
             checkout_session = stripe.checkout.Session.create(
-                success_url=domain_url + 'success?session_id={CHECKOUT_SESSION_ID}',
-                cancel_url=domain_url + 'cancelled/',
+                success_url=success_url,
+                cancel_url=cancel_url,
                 payment_method_types=['card'],
                 mode='payment',
                 line_items=[{
@@ -54,9 +59,55 @@ def create_checkout_session(request):
                         'quantity': 1,
                     }]
             )
+            print("I am here:", product)
             return JsonResponse({'sessionId': checkout_session['id']})
         except Exception as e:
             return JsonResponse({'error': str(e)})
+    elif request.method == 'GET' and product == 'beer':
+        try:
+            checkout_session = stripe.checkout.Session.create(
+                success_url=success_url,
+                cancel_url=cancel_url,
+                payment_method_types=['card'],
+                mode='payment',
+                line_items=[{
+                        'price_data':{
+                            'unit_amount':1500,
+                            'product_data':{'name': 'beer'},
+                            'currency': 'gbp',
+                                      },
+                        'quantity': 1,
+                    }]
+            )
+            print("I am here:", product)
+            return JsonResponse({'sessionId': checkout_session['id']})
+        except Exception as e:
+            return JsonResponse({'error': str(e)})
+        
+    elif request.method == 'GET' and product == 'crate':
+        try:
+            checkout_session = stripe.checkout.Session.create(
+                success_url=success_url,
+                cancel_url=cancel_url,
+                payment_method_types=['card'],
+                mode='payment',
+                line_items=[{
+                        'price_data':{
+                            'unit_amount':12000,
+                            'product_data':{'name': 'crate'},
+                            'currency': 'gbp',
+                                      },
+                        'quantity': 1,
+                    }]
+            )
+            print("I am here:", product)
+            return JsonResponse({'sessionId': checkout_session['id']})
+        except Exception as e:
+            return JsonResponse({'error': str(e)})
+    
+    else:
+        return render(request, 'payment/')
+
         
 
 @csrf_exempt
