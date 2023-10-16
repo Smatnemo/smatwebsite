6c61bbf55131091785e7327bd48d4f9d7e5db78f
Tertese Moses <Smatnemo25@gmail.com>
2023-10-16 20:20:31 +0100

    Created url for different products

--- payments/urls.py
+++ payments/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('payment/', views.HomePayView.as_view(), name='payHome'),
     path('config/', views.stripe_config),
-    path('create-checkout-session/', views.create_checkout_session),
+    path('create-checkout-session/<product>', views.create_checkout_session),
     path('success/', views.SuccessView.as_view(), name='successfulPayment'),
     path('cancelled/', views.CancelledView.as_view(), name='cancelledPayment'),
     path('webhook/', views.stripe_webhook),
